bd9f73465b6ca1fc05b0993b42ffeb50903d53cb
amulyamanjunatha9-max <amulyamanjunatha9@gmail.com>
2026-09-08 11:02:49 +0530

    Add files via upload

---
+++ class divisible.java
@@ -0,0 +1,9 @@
+class divisible {
+    public state void main (String args[]){
+        int n = 221;
+        if (n % 13 == 0){
+        if (n % 17 == 0)
+        System.out.println("Divisible by 13 and 17")
+    }
+}
+}
